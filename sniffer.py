7b2cff9938d4e381ae67bf1179ac3e3271963f06
Jeal Patel <159673874+JealPatel@users.noreply.github.com>
2025-06-02 11:27:03 +0530

    Add files via upload

---
+++ sniffer.py
@@ -0,0 +1,24 @@
+from scapy.all import sniff, IP, TCP, UDP, ICMP
+from datetime import datetime
+
+def packet_callback(packet):
+    if IP in packet:
+        ip_layer = packet[IP]
+        protocol = "OTHER"
+
+        if TCP in packet:
+            protocol = "TCP"
+        elif UDP in packet:
+            protocol = "UDP"
+        elif ICMP in packet:
+            protocol = "ICMP"
+
+        print("="*60)
+        print(f"Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+        print(f"Source IP: {ip_layer.src}")
+        print(f"Destination IP: {ip_layer.dst}")
+        print(f"Protocol: {protocol}")
+
+        
+print("Starting packet capture... Press Ctrl+C to stop.\n")
+sniff(prn=packet_callback, store=False)
